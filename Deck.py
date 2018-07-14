4f1f772f1d4e36dba79f19d184f0911987af7573
sReal <35725129+sReeall@users.noreply.github.com>
2018-07-14 19:32:39 +0100

    docstring for class card

--- Deck.py
+++ Deck.py
@@ -5,7 +5,19 @@ Created on 14 Jul 2018
 '''
 
 class card():
+    '''
+    used to represent a single card and is basic component of deck class.
     
+    constructor:
+        suit
+        rank
+    
+    attributes:
+        suit: should be one of the following 'H', 'C', 'D', 'S'
+        rank: 2 - 10, 'J', 'Q', 'K', 'A'
+        value: tuple used as rank 'A' has 1 and 11
+        
+    '''
     def __init__ (self,suit,rank):
         self.suit = suit
         self.rank = rank
@@ -21,21 +33,28 @@ class card():
     def __str__(self):
         return f'{self.rank}{self.suit}'
         
+class deck():
+    
+    def __init__(self,num=1):
         
+        pass
+    
+    
+    
 if __name__=='__main__':
     # test class card
-    print('Create a card with suit H and rank J')
-    card1 = card('H','J')
-    print(card1)
-    print(card1.value)
-    
-    print('Create a card with suit C and rank A')
-    card2 = card('C','A')
-    print(card2)
-    print(card2.value)
-    
-    print('Ceate a card with suit D and rank 2')
-    card3 = card('D','2')
-    print(card3)
-    print(card3.value)
+#     print('Create a card with suit H and rank J')
+#     card1 = card('H','J')
+#     print(card1)
+#     print(card1.value)
+#     
+#     print('Create a card with suit C and rank A')
+#     card2 = card('C','A')
+#     print(card2)
+#     print(card2.value)
+#     
+#     print('Ceate a card with suit D and rank 2')
+#     card3 = card('D','2')
+#     print(card3)
+#     print(card3.value)
     
