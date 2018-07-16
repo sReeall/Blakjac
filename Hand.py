7c364072cba471be8ab1347793babdd4285a3e04
sReal <35725129+sReeall@users.noreply.github.com>
2018-07-16 19:15:04 +0100

    added hand class which is built upon cards

---
+++ Hand.py
@@ -0,0 +1,20 @@
+'''
+A hand consists of two cards initially, more can be added if the assigned player selects hit,
+and the value is not greater then 21.
+it is assigned to a player. if it is the deal, only one card is visable.
+it will need to be compared to other hands to check if it is winning.
+if it totals 21, it is considered black jack.
+
+Created on 15 Jul 2018
+
+@author: sReeall
+'''
+
+class hand ():
+    
+    '''
+    
+    '''
+    
+    def __init__(self):
+        pass
