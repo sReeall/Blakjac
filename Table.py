6d7215367e7c6f8a583081b3c0c59a524badf676
sReal <35725129+sReeall@users.noreply.github.com>
2018-07-31 20:15:54 +0100

    added hand value to table

--- Table.py
+++ Table.py
@@ -22,16 +22,21 @@ class table():
         print(f'Current Balance: {self.player.getBalance()}')
         print('')
         print("|{:-^60}|".format(" Table "))
-        print('|{:^60}'.format('')+'|')
-        print(f'| {self.player.getName():29} {self.dealer.getName():29}|')
-        print('|{:^60}'.format('')+'|')
+        print('|{:^30}'.format('') + '|'+ '{:^29}'.format('') +'|')
+        print(f'| {self.player.getName():29}| {self.dealer.getName():28}|')
+        print('|{:^30}'.format('') + '|'+ '{:^29}'.format('') +'|')
         if (self.player.getHand()).isEmpty(): 
-            print('|{:^60}'.format('')+'|')
+            print('|{:^30}'.format('') + '|'+ '{:^29}'.format('') +'|')
         else: 
-            print(f'| {str(self.player.getHand()):29} {str(self.dealer.getHand()):29}|')
-        print('|{:^60}'.format('')+'|')
-        print('|{:^60}'.format('')+'|')
-        print('{:-^60}'.format(''))
+            print(f'| {str(self.player.getHand()):29}| {str(self.dealer.getHand()):28}|')
+        print('|{:^30}'.format('') + '|'+ '{:^29}'.format('') +'|')
+        
+        playerHandValueStr = "Hand Value: " + str(self.player.hand.getvalue())
+        dealerHandValuestr = "Hand Value: " + str(self.dealer.hand.getvalue())
+        
+        print(f'| {playerHandValueStr:29}| {dealerHandValuestr:28}|')
+#        print('|{:^60}'.format('')+'|')
+        print('|{:-^60}|'.format(''))
         print('Bet')
         print(f'{self.player.getBet()}')
     def clearBoard(self):
